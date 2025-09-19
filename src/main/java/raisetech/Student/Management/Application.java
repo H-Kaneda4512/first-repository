2398b26976858bd9864dadab8f6f5d0a62da0a67
H-Kaneda4512 <L2506933371as@gmail.com>
2025-09-19 14:12:40 +0900

    課題提出用7

--- src/main/java/raisetech/Student/Management/Application.java
+++ src/main/java/raisetech/Student/Management/Application.java
@@ -1,48 +1,51 @@
 package raisetech.Student.Management;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
-    private final Map<String, String> studentMap = new HashMap<>();
+    @Autowired
+    private StudentRepository repository;
 
 	public static void main(String[] args) {
 
 		SpringApplication.run(Application.class, args);
 	}
     @SuppressWarnings("unused")
-    @GetMapping("/studentInfo")
-    public Map<String, String> getStudentInfo() {
-        return studentMap;
+    @GetMapping("/student")
+    public String getStudent(@RequestParam String name) {
+        Student student = repository.searchByName(name);
+        return student.getName() + " " + student.getAge() + "歳";
     }
 
     @SuppressWarnings("unused")
-    @PostMapping("/studentInfo")
-    public String setStudentInfo(@RequestParam String name, @RequestParam String age) {
-        studentMap.put(name, age);
-        return name + " を登録しました（年齢: " + age + "）";
+    @GetMapping("/students")
+    public List<Student>getAllStudents() {
+        return repository.findAll();
     }
 
+    @SuppressWarnings("unused")
+    @PostMapping("/student")
+    public void registerStudent(@RequestParam String name, @RequestParam int age) {
+      repository.registerStudent(name, age);
+    }
 
     @SuppressWarnings("unused")
-    @PostMapping("/studentName")
-    public String updateStudentName(@RequestParam String oldName, @RequestParam String newName) {
-        if (studentMap.containsKey(oldName)) {
-            String age = studentMap.remove(oldName);
-            studentMap.put(newName, age);
-            return oldName + " → " + newName + " に変更しました";
-        } else {
-            return oldName + " のデータがありません";
-        }
+    @PatchMapping("/student")
+    public void updateStudent(@RequestParam String name, @RequestParam int age) {
+      repository.updateStudent(name, age);
+    }
+
+    @SuppressWarnings("unused")
+    @DeleteMapping("/student")
+    public void deleteStudent(@RequestParam String name) {
+       repository.deleteStudent(name);
     }
 }
